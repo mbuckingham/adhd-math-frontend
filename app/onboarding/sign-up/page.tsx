d505953f14ade0050f5d223bb1af57ff359c76f4
vchlinnn <chuclinh190305@gmail.com>
2024-07-12 17:24:02 -0500

    Style sign up page

--- app/onboarding/sign-up/page.tsx
+++ app/onboarding/sign-up/page.tsx
@@ -2,63 +2,85 @@ import React from "react";
 
 export default function SignUp() {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <div className="bg-white p-8 rounded-lg shadow-lg max-w-md w-full">
-        <h2 className="text-2xl font-bold mb-6">Create Account</h2>
-        <form>
-          <div className="mb-4">
-            <label
-              htmlFor="email"
-              className="block text-sm font-medium text-gray-700"
-            >
-              Email
-            </label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              className="mt-1 p-2 block w-full border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-              placeholder="abc@gmail.com"
-            />
-          </div>
-          <div className="mb-4">
-            <label
-              htmlFor="password"
-              className="block text-sm font-medium text-gray-700"
-            >
-              Password
-            </label>
-            <input
-              type="password"
-              id="password"
-              name="password"
-              className="mt-1 p-2 block w-full border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-              placeholder="********"
-            />
-          </div>
-          <div className="flex items-center justify-between mb-6">
-            <p className="text-sm text-gray-600">
-              <span>✔ One lowercase character</span>
-              <br />
-              <span>✔ One uppercase character</span>
-              <br />
-              <span>✔ One special character</span>
-              <br />
-              <span>✔ 8 characters minimum</span>
+    <div className="bg-white p-8 rounded-lg max-w-lg w-full mx-auto mt-10">
+      <h2 className="text-3xl font-bold mb-6 text-gray-700">Create Account</h2>
+      <form>
+        <div className="mb-6">
+          <label
+            htmlFor="email"
+            className="block text-lg font-bold text-gray-700"
+          >
+            Email
+          </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            className="mt-2 p-2 block w-full border border-gray-300 rounded-md shadow-sm focus:ring-gray-400 focus:border-gray-400 sm:text-sm"
+            placeholder="abc@gmail.com"
+          />
+        </div>
+        <div className="mb-2">
+          <label
+            htmlFor="password"
+            className="block text-lg font-bold text-gray-700"
+          >
+            Password
+          </label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            className="mt-2 p-2 block w-full border border-gray-300 rounded-md shadow-sm focus:ring-gray-400 focus:border-gray-400 sm:text-sm"
+            placeholder="********"
+          />
+        </div>
+        <div className="flex items-center justify-between mb-8">
+          <div className="pl-2 w-1/2">
+            <p className="text-sm text-gray-600 mb-1 w-full">
+              ✔ One lowercase character
+            </p>
+            <p className="text-sm text-gray-600 w-full">
+              ✔ One special character
             </p>
           </div>
-          <button
-            type="submit"
-            className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          <div className="pr-2 w-1/2">
+            <p className="text-sm text-gray-600 mb-1 w-full">
+              ✔ One uppercase character
+            </p>
+            <p className="text-sm text-gray-600 w-full">
+              ✔ 8 characters minimum
+            </p>
+          </div>
+        </div>
+        <button
+          type="submit"
+          className="max-w-xs flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-md font-bold text-gray-800 bg-gray-400 hover:bg-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300"
+        >
+          Continue
+          <svg
+            className="ml-2 h-5 w-5"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
           >
-            Continue
-          </button>
-          <p className="mt-6 text-sm text-gray-600">
-            Note: You can always go into <strong>Settings</strong> if you want
-            to change your password.
-          </p>
-        </form>
-      </div>
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M9 5l7 7-7 7"
+            />
+          </svg>
+        </button>
+        <p
+          className="mt-8 text-sm text-gray-600 whitespace-nowrap tracking-tight"
+          style={{ fontSize: "13px" }}
+        >
+          Note: You can always go into <strong>Settings</strong> if you want to
+          change your password.
+        </p>
+      </form>
     </div>
   );
 }
