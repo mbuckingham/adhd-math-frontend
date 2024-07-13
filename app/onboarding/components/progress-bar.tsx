977a8422fb9525748d69a1d484972fbea4e79286
Nat-crit20 <greuelnathan74@gmail.com>
2024-07-13 17:27:45 -0500

    Updated the padding for the p elements

--- app/onboarding/components/progress-bar.tsx
+++ app/onboarding/components/progress-bar.tsx
@@ -4,12 +4,12 @@ import Check from "../../../public/onboarding/Icon/CheckMark.svg";
 
 export default function ProgressBar() {
   return (
-    <div className="flex items-center space-x-4">
-      <div className="flex flex-col items-center space-x-4">
+    <div className="flex items-center ">
+      <div className="flex flex-col items-center space-x-5 ">
         <div className="flex  h-9 p-2 justify-center items-center rounded-full border-2 border-gray-700 bg-gray-400 grow-0 shrink-0">
           <Image src={Check} alt="completed" width={15} height={15} />
         </div>
-        <p>Me</p>
+        <p className="pr-5">Me</p>
       </div>
 
       <div className="w-full bg-gray-200 rounded-full h-1.5  dark:bg-gray-700">
@@ -18,11 +18,11 @@ export default function ProgressBar() {
           style={{ width: "100%" }}
         ></div>
       </div>
-      <div className="flex flex-col items-center space-x-4">
+      <div className="flex flex-col items-center space-x-5 ">
         <div className="flex  h-9 p-2 justify-center items-center rounded-full border-2 border-gray-700 bg-gray-400 grow-0 shrink-0">
           <Image src={Check} alt="completed" width={15} height={15} />
         </div>
-        <p>Lessons</p>
+        <p className="pr-5">Lessons</p>
       </div>
 
       <div className="w-full bg-gray-200 rounded-full h-1.5  dark:bg-gray-700">
@@ -31,11 +31,11 @@ export default function ProgressBar() {
           style={{ width: "100%" }}
         ></div>
       </div>
-      <div className="flex flex-col items-center space-x-4">
+      <div className="flex flex-col items-center space-x-5 ">
         <div className="flex w-9 h-9 p-3 justify-center items-center gap-2.5 rounded-full border-2 border-gray-700 bg-gray-400">
           #
         </div>
-        <p>Settings</p>
+        <p className="pr-5">Settings</p>
       </div>
 
       <div className="w-full bg-gray-200 rounded-full h-1.5 dark:bg-gray-700">
@@ -44,11 +44,11 @@ export default function ProgressBar() {
           style={{ width: "45%" }}
         ></div>
       </div>
-      <div className="flex flex-col items-center space-x-4">
+      <div className="flex flex-col items-center space-x-5 ">
         <div className="flex w-9 h-9 p-3 justify-center items-center gap-2.5 rounded-full border-2 border-gray-600 bg-gray-300">
           #
         </div>
-        <p>Buddy</p>
+        <p className="pr-5">Buddy</p>
       </div>
     </div>
   );
