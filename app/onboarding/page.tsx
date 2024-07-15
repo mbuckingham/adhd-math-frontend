3f2370335f302d7ccfedad36082c87b629033a61
Lilibeth Neal <mejialily036@gmail.com>
2024-07-15 15:52:20 -0500

    added background image to welcome pg

--- app/onboarding/page.tsx
+++ app/onboarding/page.tsx
@@ -1,8 +1,10 @@
 import ProgressBar from "./components/progress-bar";
+import Welcome from "./welcome/welcome";
 
 export default function Page() {
   return (
     <div>
+      <Welcome />
       <ProgressBar />
       <h1>Hello World</h1>
     </div>
